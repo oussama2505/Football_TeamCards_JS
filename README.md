150a04728f5e1c7065e53a1edacdb6bc475496c0
oussama2505 <oussama.brahmi0608@gmail.com>
2024-03-21 10:37:33 +0100

    Filters Function

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Football_TeamCards_JS
+# Learn Modern JavaScript Methods by Building Football Team Cards
+In this project, you will build a set of football team cards and learn about nested objects, object destructuring, default parameters, event listeners, and switch statements
+
+## Use the Object.freeze() method
+To ensure that you can't modify an object by adding or removing any properties. We are going to use a method called Object.freeze(obj) which will freeze this object and prevent any changes being made to it.
+
+## object destructuring
+The object destructuring syntax allows you to unpack values from arrays and objects:
+
+`
+const developerObj = {
+  name: "Jessica Wilkins",
+  isDeveloper: true
+};
+
+// Object destructuring
+const { name, isDeveloper } = developerObj;
+`
+
+## function Filter()
+- Now you will start building out the function that will show player cards based on the selections made by the user in the Filter Teammates dropdown menu.
+
+- Function parameters can be initialized with default values. If a function is called without an argument, then the default value will be used:
+
+`
+const greeting = (name = "Anonymous") => {
+  return "Hello " + name;
+} 
+
+console.log(greeting("John")); // Hello John
+console.log(greeting()); // Hello Anonymous
+`
+
+- arr contains a series of objects that each contains a name, position, number, isCaptain and nickname property. In order to access each of those properties inside the callback function, you will need to use object destructuring to unpack them into variables.
+
+- Inside the body of the callback function, you will need to add template literals `` which will contain the HTML content for the player cards.
+
+Since you are working with template literals, you will need to use an embedded expression for the name parameter
+
+`${expression goes here`
+
+- The .map() method will return a new array of player-card items separated by commas.
+
+To remove the commas between each player-card so it does not show up on screen, chain the .join() method to the .map() method. Pass an empty string as the argument for the .join() method.
---
+++ script.js
@@ -0,0 +1,209 @@
+const teamName=document.getElementById('team');
+const typeOfSport=document.getElementById('sport');
+const worldCupYear=document.getElementById('year');
+const headCoach=document.getElementById('head-coach');
+const playerCards=document.getElementById('player-cards');
+const playersDropdownList=document.getElementById('players');
+
+
+const myFavoriteFootballTeam={
+    team:"Argentina",
+    sport:"Football",
+    year:1986,
+    isWorldCupWinner:true,
+    headCoach:{
+        coachName:"Carlos Bilardo",
+        matches:7
+    },
+    players:[
+        {
+            name: "Sergio Almirón",
+            position: "forward",
+            number: 1,
+            isCaptain: false,
+            nickname: null
+        },
+        {
+            name: "Sergio Batista",
+            position: "midfielder",
+            number: 2,
+            isCaptain: false,
+            nickname: null ,
+        },
+        {
+            name: "Ricardo Bochini",
+            position: "midfielder",
+            number: 3,
+            isCaptain: false,
+            nickname: "El Bocha",
+          },
+          {
+            name: "Claudio Borghi",
+            position: "midfielder",
+            number: 4,
+            isCaptain: false,
+            nickname: "Bichi",
+          },
+          {
+            name: "José Luis Brown",
+            position: "defender",
+            number: 5,
+            isCaptain: false,
+            nickname: "Tata",
+          },
+          {
+            name: "Daniel Passarella",
+            position: "defender",
+            number: 6,
+            isCaptain: false,
+            nickname: "El Gran Capitán",
+          },
+          {
+            name: "Jorge Burruchaga",
+            position: "forward",
+            number: 7,
+            isCaptain: false,
+            nickname: "Burru",
+          },
+          {
+            name: "Néstor Clausen",
+            position: "defender",
+            number: 8,
+            isCaptain: false,
+            nickname: null,
+          },
+          {
+            name: "José Luis Cuciuffo",
+            position: "defender",
+            number: 9,
+            isCaptain: false,
+            nickname: "El Cuchu",
+          },
+          {
+            name: "Diego Maradona",
+            position: "midfielder",
+            number: 10,
+            isCaptain: true,
+            nickname: "El Pibe de Oro",
+          },
+          {
+            name: "Jorge Valdano",
+            position: "forward",
+            number: 11,
+            isCaptain: false,
+            nickname: "The Philosopher of Football",
+          },
+          {
+            name: "Héctor Enrique",
+            position: "midfielder",
+            number: 12,
+            isCaptain: false,
+            nickname: null,
+          },
+          {
+            name: "Oscar Garré",
+            position: "defender",
+            number: 13,
+            isCaptain: false,
+            nickname: null,
+          },
+          {
+            name: "Ricardo Giusti",
+            position: "midfielder",
+            number: 14,
+            isCaptain: false,
+            nickname: null,
+          },
+          {
+            name: "Luis Islas",
+            position: "goalkeeper",
+            number: 15,
+            isCaptain: false,
+            nickname: "El loco",
+          },
+          {
+            name: "Julio Olarticoechea",
+            position: "defender",
+            number: 16,
+            isCaptain: false,
+            nickname: null,
+          },
+          {
+            name: "Pedro Pasculli",
+            position: "forward",
+            number: 17,
+            isCaptain: false,
+            nickname: null,
+          },
+          {
+            name: "Nery Pumpido",
+            position: "goalkeeper",
+            number: 18,
+            isCaptain: false,
+            nickname: null,
+          },
+          {
+            name: "Oscar Ruggeri",
+            position: "defender",
+            number: 19,
+            isCaptain: false,
+            nickname: "El Cabezón",
+          },
+          {
+            name: "Carlos Tapia",
+            position: "midfielder",
+            number: 20,
+            isCaptain: false,
+            nickname: null,
+          },
+          {
+            name: "Marcelo Trobbiani",
+            position: "midfielder",
+            number: 21,
+            isCaptain: false,
+            nickname: "Calesita",
+          },
+          {
+            name: "Héctor Zelada",
+            position: "goalkeeper",
+            number: 22,
+            isCaptain: false,
+            nickname: null,
+          },
+    ],
+};
+
+//freeze this object and prevent any changes being made to it
+Object.freeze(myFavoriteFootballTeam);
+
+/* const sport=myFavoriteFootballTeam.sport;
+const team=myFavoriteFootballTeam.team; */
+
+const {sport,team,year,players}=myFavoriteFootballTeam;
+//using the destructuring syntax
+const {coachName}=myFavoriteFootballTeam.headCoach;
+
+
+// displaying the team's information on the screen
+typeOfSport.textContent=sport;
+teamName.textContent=team;
+worldCupYear.textContent=year;
+headCoach.textContent=coachName;
+
+//building out the function that will show player cards based on the selections made by the user
+const setPlayerCards=(arr=players) => {//Function parameters can be initialized with default values.
+
+    playerCards.innerHTML+=arr.map(({name,position,number,isCaptain,nickname})=>{// create a new array that will be responsible for adding the player card information to the page
+
+        `
+            <div class="player-card">
+            <h2>${name}${isCaptain ? " (Captain)" : ""}</h2>
+            <p>Position:${position}</p>
+            <p>Number: ${number}</p>
+            <p>Nickname: ${nickname?nickname:"N/A"}</p>
+            </div>
+        `
+
+    }).join("");//To remove the commas between each player-card so it does not show up on screen
+
+}
